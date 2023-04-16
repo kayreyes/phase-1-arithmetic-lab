b3c15f082170b89649baea3c067262d1fe033ad4
Kayla Reyes <kayla.reyes10.kr@gmail.com>
2023-04-16 00:10:53 -0500

    Completed phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+var multiply;
+var num1=2;
+var num2=31;
+multiply = num1 * num2; 
+
+var random= 0; 
+random = Math.floor(Math.random() * 10) + 1;
+
+var mod; 
+var num3=22;
+var num4=6; 
+mod = num3 % num4; 
+
+var max = 0;
+max = Math.max(5,10, 15, 20);
